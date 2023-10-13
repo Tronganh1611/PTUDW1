578ca12d04b6d724e4008a9a9c2c091df8b81905
Tronganh1611 <tronganh16112003@gmail.com>
2023-10-13 21:10:12 +0700

    bai 2 3

---
+++ 63CNTT4/63CNTT4/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyClass.Model;
+using MyClass.DAO;
+
+namespace _63CNTT4.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesDAO categoriesDAO = new CategoriesDAO();
+        /// //////////////////////////////////////////////////////////
+        ///INDEX
+
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            return View(categoriesDAO.getList("Index"));//chi hien thi cac dong co status == 1,2
+        }
+        /// //////////////////////////////////////////////////////////
+        ///INDEX
+
+        // GET: Admin/Category/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+        /// //////////////////////////////////////////////////////////
+        ///INDEX
+
+        // GET: Admin/Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Category/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                categoriesDAO.Insert(categories);
+                return RedirectToAction("Index");
+            }
+
+            return View(categories);
+        }
+        /// //////////////////////////////////////////////////////////
+        ///EDIT
+
+        // GET: Admin/Category/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            // tim dong DB can chinh sua
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: Admin/Category/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Slug,ParentID,Order,MetaDesc,MetaKey,CreateAt,CreateBy,UpdateAt,UpdateBy,Status")] Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                categoriesDAO.Update(categories);
+                return RedirectToAction("Index");
+            }
+            return View(categories);
+        }
+
+        // GET: Admin/Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+           
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: Admin/Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Categories categories = categoriesDAO.getRow(id);
+            categoriesDAO.Delete(categories);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 63CNTT4/MyClass/DAO/CategoriesDAO.cs
@@ -0,0 +1,84 @@
+﻿using MyClass.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass.DAO
+{
+    public class CategoriesDAO
+    {
+
+        private MyDBContext db = new MyDBContext();
+
+        //////////////////////////////////////////////
+        ///INDEX
+        public List<Categories> getList(int? id)
+        {
+            return db.Categories.ToList();
+        }
+        /// //////////////////////////////////////////////////////////
+        ///INDEX voi gia tri Status 1,2 - 0: An khoi trang giao dien
+        public List<Categories> getList(string status = "All")
+        {
+            List<Categories> list = null;
+            switch (status)
+            {
+                case "Index":
+                    {
+                        list = db.Categories.Where(m => m.Status != 0).ToList()
+;
+                        break;
+                    }
+                case "Trash": //statu == 0
+                    {
+                        list = db.Categories.Where(m => m.Status == 0).ToList();
+                        break;
+                    }
+                default:
+                    {
+                        list = db.Categories.ToList();
+                        break;
+                    }
+            }
+            return list;
+        }
+        /// //////////////////////////////////////////////////////////
+        ///DETAILS hien th 1 dong du lieu
+        public Categories getRow(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+            else
+            {
+                return db.Categories.Find(id);
+            }
+        }
+        /// //////////////////////////////////////////////////////////
+        ///CREATE = Insert 1 dong DB
+        public int Insert(Categories row)
+        {
+            db.Categories.Add(row);
+            return db.SaveChanges();
+        }
+
+        /// //////////////////////////////////////////////////////////
+        ///EDIT = Update 1 dong DB
+        public int Update(Categories row)
+        {
+            db.Entry(row).State = EntityState.Modified;
+            return db.SaveChanges();
+        }
+        /// //////////////////////////////////////////////////////////
+        ///DELETE = Update 1 dong 
+        public int Delete(Categories row)
+        {
+            db.Categories.Remove(row);
+            return db.SaveChanges();
+        }
+    }
+}
